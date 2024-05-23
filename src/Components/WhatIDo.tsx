a3064b9be2d615c60a294d9cf3536fec4cc379f5
emilyftaylor <emily@compose.im>
2024-05-23 21:25:09 +0100

    Updates

--- src/Components/WhatIDo.tsx
+++ src/Components/WhatIDo.tsx
@@ -4,7 +4,9 @@ import React from 'react';
 const WhatIDo: React.FC = () => {
   return (
     <div className="playfair-regular text-center items-center justify-center mt-48 bg-cover bg-center flex flex-col w-3/5 ">
-    <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+       <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl ">   
+What I Do</h1>
+<h1 className="text-xl font-bold tracking-tight text-gray-900 sm:text-3xl mt-6">   
     I have helped people from all walks of life navigate grief and loss, anxiety, depression, work-stress, ME/Chronic Fatigue, women’s and relationship issues. 
 </h1>
 <p className="mt-6 text-lg leading-8 text-gray-600">
