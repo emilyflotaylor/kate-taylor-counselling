2f5d6974a776c7f259fe3dd3512e8ae8088c13a3
emilyftaylor <emily@compose.im>
2024-06-14 22:17:28 +0100

    Update Footer.tsx

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,7 +3,7 @@ import logoImage from '../assets/AdobeStock_125867356.jpeg';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-white dark:bg-gray-900">
+    <footer className="bg-white">
       <div className="mx-auto w-full max-w-screen-xl flex justify-center py-6 mb-12">
         <img className='w-1/3 md:w-1/6 lg:w-1/8' src={logoImage} alt="Logo" />
       </div>
